55b8111f8842d9a7bf1f87a5c796422dbd444bf3
Dennis Hieber <d.hieber92@web.de>
2016-04-16 16:11:13 +0200

    add postprocessing

---
+++ PostProcessing.py
@@ -0,0 +1,25 @@
+import re
+
+# Post-Processing
+
+
+def correctDate(date):
+    isDate(date)
+    return date
+
+
+def isDate(str):
+    regexDate = re.compile(r"\b(\d{1,2}[-/:.]){1,2}\d{2,4}\b")
+    if regexDate.match(str) is None:
+        print "Fehler: Kein korrektes Datum"
+    else:
+        print "korrektes Datum"
+
+
+def testIsDate():
+    dates = ["10/03/2016", "10.03.2016", "10.03.16", "03-17"]
+    for date in dates:
+        isDate(date)
+
+if __name__ == "__main__":
+    testIsDate()
