e306155a1586bcf601f63e6136a5d64d15191ee7
gauurof <cihan.aktas@gmail.com>
2016-04-16 16:22:11 +0200

    cleaned

--- Gendata.py
+++ Gendata.py
@@ -47,11 +47,6 @@ def main():
 
     intClassifications = []         # declare empty classifications list, this will be our list of how we are classifying our chars from user input, we will write to file at the end
 
-                                    # possible chars we are interested in are digits 0 through 9, put these in list intValidChars
-
-    #ord('A'), ord('B'), ord('C'), ord('D'), ord('E'), ord('F'), ord('G'), ord('H'), ord('I'), ord('J'),
-     #                ord('K'), ord('L'), ord('M'), ord('N'), ord('O'), ord('P'), ord('Q'), ord('R'), ord('S'), ord('T'),
-      #               ord('U'), ord('V'), ord('W'), ord('X'), ord('Y'), ord('Z'),
     intValidChars = [ord('0'), ord('1'), ord('2'), ord('3'), ord('4'), ord('5'), ord('6'), ord('7'), ord('8'), ord('9'),
 ord('/'), ord('.')]
 
