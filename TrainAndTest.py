4086f63f86a272116f79a0919b10992b66fa6371
gauurof <cihan.aktas@gmail.com>
2016-04-19 12:58:49 +0200

    svm gelöscht

--- TrainAndTest.py
+++ TrainAndTest.py
@@ -62,7 +62,7 @@ def main():
 
     kNearest.train(npaFlattenedImages, cv2.ml.ROW_SAMPLE, npaClassifications)
 
-    imgTestingNumbers = cv2.imread("TestBilder/testDatum7.png")          # read in testing numbers image
+    imgTestingNumbers = cv2.imread("TestBilder/testDatum11.png")          # read in testing numbers image
 
     if imgTestingNumbers is None:                           # if image was not read successfully
         print "error: image not read from file \n\n"        # print error message to std out
